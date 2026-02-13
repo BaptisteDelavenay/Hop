4be766db3d02af7f5b066298c7488ae76f50c7cb
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-13 15:21:19 +0100

    Ajout style au formulaire de conneixon

--- compte/connexion.php
+++ compte/connexion.php
@@ -7,20 +7,43 @@
     <link href="../ASSETS/dist/output.css" rel="stylesheet">
 </head>
 <body>
-    <p>Connexion</p>
+    <p class="text-center">Connexion</p>
     <img src="../IMG/Logo_png.png" alt="">
-    <form action="verifConnexion.php" method="POST">
-        <label for="email">Email professionel</label>
-        <input type="email" id="email" name="email">
+    <form class="flex flex-col justify-center items-center" action="verifConnexion.php" method="POST">
 
-        <label for="password">Mot de passe</label>
-        <input type="password" id="password" name="password">
+        <div class="flex w-11/12 p-1 bg-gray-200 rounded-xl">
+    
+            <label class="flex-1 cursor-pointer">
+                <input type="radio" name="user_type" value="collaborateur" class="sr-only peer" checked>
+                <div class="flex items-center justify-center py-2 text-sm font-semibold text-gray-600 transition-all rounded-lg peer-checked:bg-white peer-checked:text-gray-900 peer-checked:shadow-sm">
+                    Collaborateur
+                </div>
+            </label>
 
-        <a href="">Mot de passe oublié ?</a>
+            <label class="flex-1 cursor-pointer">
+                <input type="radio" name="user_type" value="entreprise" class="sr-only peer">
+                <div class="flex items-center justify-center py-2 text-sm font-semibold text-gray-600 transition-all rounded-lg peer-checked:bg-white peer-checked:text-gray-900 peer-checked:shadow-sm">
+                    Entreprise
+                </div>
+            </label>
+            
+        </div>
 
-        <input type="submit" value="Se connecter">
 
-        <p>Vous n'avez pas de compte ? <a href="">Créez-en un !</a></p>
+        <div class="w-11/12 flex flex-col">
+            <label for="email">Email professionel</label>
+            <input class="bg-gray-200 h-10 rounded-lg" type="email" id="email" name="email">
+        </div>
+
+        <div class="w-11/12 flex flex-col">
+            <label for="password">Mot de passe</label>
+            <input class="bg-gray-200 h-10 rounded-lg" type="password" id="password" name="password">
+        </div>
+        <a href="" class="">Mot de passe oublié ?</a>
+
+        <input type="submit" value="Se connecter" class="w-11/12 bg-gray-400 h-10 rounded-lg">
+
+        <p class="text-center">Vous n'avez pas de compte ? <a href="">Créez-en un !</a></p>
     </form>
 </body>
 </html>
