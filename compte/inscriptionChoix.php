2073f575d0cf97e2dd09aac5d2066b83bfffee31
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-13 22:57:37 +0100

    maj des liens de inscriptionChoix.php

--- compte/inscriptionChoix.php
+++ compte/inscriptionChoix.php
@@ -15,7 +15,7 @@
     </div>
 
     <!-- Carte création de compte collaborateur -->
-    <a href="" class="block bg-gray-200 h-60 w-11/12 m-auto mt-10 p-2 rounded-md">
+    <a href="inscriptionCollaborateur.php" class="block bg-gray-200 h-60 w-11/12 m-auto mt-10 p-2 rounded-md">
         <div class="flex items-center justify-between">
             <div class="bg-gray-400 p-1 rounded-md">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>
@@ -35,7 +35,7 @@
     </a>
 
     <!-- Carte création de compte entreprise -->
-    <a href="" class="block bg-gray-200 h-60 w-11/12 m-auto mt-10 p-2 rounded-md">
+    <a href="inscriptionEntreprise.php" class="block bg-gray-200 h-60 w-11/12 m-auto mt-10 p-2 rounded-md">
         <div class="flex items-center justify-between">
             <div class="bg-gray-400 p-1 rounded-md">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" /></svg>
