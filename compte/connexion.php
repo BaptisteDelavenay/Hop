efd204d7bc124e0412f0868ec7c5868f156bad17
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-13 14:36:01 +0100

    formulaire de connexion

--- compte/connexion.php
+++ compte/connexion.php
@@ -7,6 +7,20 @@
     <link href="../ASSETS/dist/output.css" rel="stylesheet">
 </head>
 <body>
-    
+    <p>Connexion</p>
+    <img src="../IMG/Logo_png.png" alt="">
+    <form action="verifConnexion.php" method="POST">
+        <label for="email">Email professionel</label>
+        <input type="email" id="email" name="email">
+
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" name="password">
+
+        <a href="">Mot de passe oublié ?</a>
+
+        <input type="submit" value="Se connecter">
+
+        <p>Vous n'avez pas de compte ? <a href="">Créez-en un !</a></p>
+    </form>
 </body>
 </html>
