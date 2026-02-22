e986153fb0793268a5491da32253ecce735032e6
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-22 21:21:54 +0100

    création index.php

--- index.php
+++ index.php
@@ -6,7 +6,20 @@
     <title>Hop</title>
     <link href="ASSETS/dist/output.css" rel="stylesheet">
 </head>
-<body class="bg-hop-neon">
-    
+<body class="bg-hop-neon p-4">
+
+    <div class="w-full flex items-center justify-center">
+        <img class="w-50" src="IMG/logoHop.svg" alt="">
+    </div>
+
+    <div class="mt-12">
+        <h2 class="text-hop-noir text-4xl font-semibold">Faites de votre engagement écologique un jeu d'équipe</h2>
+    </div>
+
+    <div class="mt-30">
+        <a href=""><button class="bg-hop-noir text-white font-semibold w-full h-14 rounded-lg">Créer mon compte</button></a>
+        <a href="compte/connexion.php"><button class="bg-hop-violet text-white font-semibold w-full h-14 rounded-lg mt-4">Se connecter</button></a>
+    </div>
+
 </body>
 </html>
