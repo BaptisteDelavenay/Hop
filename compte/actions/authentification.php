8789aaa506c95f5b83a38dcbe75a5634ed9d1bf3
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-25 16:53:57 +0100

    amélioration et simplification du code de l'authentification

--- compte/actions/authentification.php
+++ compte/actions/authentification.php
@@ -1,99 +1,37 @@
 <?php
-    
-function verifMdp(){
+
+    session_start();
 
     include "../../connexionBDD/connexionBDD.php";
 
     $email = htmlentities($_POST["email"]);
-    $MDP = $_POST["password"];
+    $password = $_POST["password"];
     $userType = $_POST["user_type"];
 
+    // Si le type de l'utilisateur est entreprise, alors table = 'entreprise', sinon table = 'user'
+    $table = ($userType === "entreprise") ? "entreprise" : "user";
+
     // Requete sql pour aller chercher tous les utilisateur pour vérifier plus tard si ils existent
-    $SelectUser = $db->prepare("SELECT * FROM user WHERE Email = :email");
-    $SelectUser->execute(array(
+    $selectAccount = $db->prepare("SELECT * FROM $table WHERE Email = :email");
+    $selectAccount->execute(array(
         "email"=>$email
     )); 
-    $users = $SelectUser->fetchAll(PDO::FETCH_ASSOC);
+    $account = $selectAccount->fetch(PDO::FETCH_ASSOC);
 
-    // Requete sql pour aller chercher toutes les entreprises pour vérifier plus tard si ils existent
-    $SelectEntreprise = $db->prepare("SELECT * FROM entreprise WHERE Email = :email");
-    $SelectEntreprise->execute(array(
-        "email"=>$email
-    )); 
-    $entreprises = $SelectEntreprise->fetchAll(PDO::FETCH_ASSOC);
-
-        
-    // Si le user type est collaborateur, on regarde si le compte existe dans la table entreprise
-    if($userType=="collaborateur"){
-
-        if ($users) {
-
-                // Le mot de passe qui est stocké dans la bdd
-                $PasswordHash = $users[0]["password"];
-
-                if (password_verify($MDP, $PasswordHash)){
-
-                    if ($users[0]["role"]=="collaborateur" && $userType=="collaborateur") {
-
-                        // Démarre une Session en tant que collaborateur
-                        session_start();
-                        $_SESSION['session_collaborateur']='OK'; 
-                        $_SESSION['user_id']=$users[0]['id'];
-                        header("Location: ../../app/views/accueilCollaborateur.php");
-                    }
-                    // Si les roles ne sont pas les bons
-                    else{
-                        echo "role incorrect";
-                    }
-                }
-
-                else {
-                    // header("Location: Connexion.php?erreurIdentifiant=identifiantincorrect");
-                    echo ("identifiant ou mot de passe incorrect");
-                };      
+    // Si le compte existe et que le mdp est correct
+    if($account && password_verify($password, $account["password"])){
+        // Si le role de la bdd correspond avec le role du formulaire
+        if($account['role'] === $userType){
+            $_SESSION['session_'.$userType]='OK';
+            $_SESSION['user_id'] = $account['id'];
+            // Choisi le fichier vers lequel l'utilisateur sera renvoyé en fonction de son rôle
+            $redirection = ($userType === "entreprise") ? "accueilEntreprise.php" : "accueilCollaborateur.php";
+            header("Location: ../../app/views/".$redirection);
+            exit();
         }
     }
-
-    // Si le user type est entreprise, on regarde si le compte existe dans la table entreprise
-    elseif($userType=="entreprise"){
-
-        if ($entreprises) {
-
-                // Le mot de passe qui est stocké dans la bdd
-                $PasswordHash = $entreprises[0]["password"];
-
-                if (password_verify($MDP, $PasswordHash)){
-
-                    if ($entreprises[0]["role"]=="entreprise" && $userType=="entreprise"){
-
-                        // Démarre une session en tant qu'entreprise
-                        session_start();
-                        $_SESSION['session_entreprise']='OK';
-                        $_SESSION['entreprise_id']=$entreprises[0]['id']; 
-                        header("Location: ../../app/views/accueilEntreprise.php");
-                        echo "Session entreprise";
-
-                    }
-                    // Si les roles ne sont pas les
-                    else{
-                        echo "role incorrect";
-                    }
-                }
-
-                else {
-                    // header("Location: Connexion.php?erreurIdentifiant=identifiantincorrect");
-                    echo ("identifiant ou mot de passe incorrect");
-                };      
-        }
-    }
-      
     else{
-        // header("Location: Connexion.php?erreurIdentifiant=identifiantincorrect");
-        echo ("Compte inconnu");
-    };
-
-  };
-
-verifMdp();
+        echo "identifiant ou mot de passe incorrect";
+    }
 
 ?>
