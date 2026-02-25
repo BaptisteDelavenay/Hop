4d0cd9289f3944fae108a1c867854c76cfc16e1d
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-25 18:45:42 +0100

    Affichage de tous les collaborateurs de son entreprise lors de la connexion à la page entreprise

--- app/views/accueilEntreprise.php
+++ app/views/accueilEntreprise.php
@@ -8,15 +8,36 @@
 <body>
 
     <?php
+        include "../../connexionBDD/connexionBDD.php";
+
         session_start();
-            if ($_SESSION['session_entreprise']!='OK') {
-                // header("Location: ../../compte/views/connexion.php");
-                echo "erreur de session";
-            };
+
+        if ($_SESSION['session_entreprise']!='OK') {
+            // header("Location: ../../compte/views/connexion.php");
+            echo "erreur de session";
+        };
+
+        // print_r($_SESSION);
     ?>
 
     <h1>Bienvenue sur l'accueil du compte entreprise !</h1>
     <a href="../../compte/actions/deconnexion.php">Deconnexion</a>
+
+    <?php
+    
+        $selectUserEntreprise = "SELECT user.id, user.prenom, user.nom FROM `user` INNER JOIN `entreprise` ON user.entreprise_id = entreprise.id WHERE entreprise.id = :id;";
+        $userEntreprise = $db->prepare($selectUserEntreprise);
+        $userEntreprise->execute(array(
+            'id'=>$_SESSION["entreprise_id"]
+        ));
+
+        $users = $userEntreprise->fetchAll(PDO::FETCH_ASSOC);
+    
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
+
+    ?>
     
 </body>
 </html>
