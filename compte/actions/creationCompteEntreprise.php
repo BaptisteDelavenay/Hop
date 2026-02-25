40c9c38e0cfd565c50bfb0b8fb7025d67386747f
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-02-25 14:57:39 +0100

    dev de l'ajout du nouveau compte dans la bdd

--- compte/actions/creationCompteEntreprise.php
+++ compte/actions/creationCompteEntreprise.php
@@ -9,8 +9,42 @@
     $activite = $_SESSION["tempo"]["activite"];
     $email = $_SESSION["tempo"]["email"];
     $password = $_SESSION["tempo"]["password"];
-    $logo = $_POST["photoDeProfil"];
+    // $photo = $_POST["photoDeProfil"];
+    $photo = "defaut.png";
 
+
+    // Vérifier si le compte existe déjà dans la bdd
+    $verifNewEntreprise = "SELECT EXISTS(SELECT 1 FROM entreprise WHERE nom = :nom AND secteur_activite = :activite AND email = :email);";
+    $verifEntreprise = $db->prepare($verifNewEntreprise);
+    $verifEntreprise->execute(array(
+        'nom' => $nomEntreprise,
+        'activite' => $activite,
+        'email' => $email
+    ));
+
+    $exists = $verifEntreprise->fetchColumn();
+
+    // Vérifie si le compte existe déjà
+    if ($exists) {
+        echo "compte deja existant !";
+        // header("Location: Connexion.php?CompteExistant");
+    } 
+
+
+    // Si il n'existe pas, on le créé
+    else {
+        
+        $nouvelleEntreprise = $db->prepare("INSERT INTO entreprise (nom, secteur_activite, email, mdp, photo_profil, total_points, niveau_arene) VALUES (:nom, :activite, :email, :password, :photo, 0, 1)");
+        $nouvelleEntreprise->execute(array(
+            'nom'     => $nomEntreprise,
+            'activite' => $activite,
+            'email'   => $email,
+            'password' => $password,
+            'photo'   => $photo
+        ));
+
+        echo "compte ajouté !";
+    };
 
 
 
