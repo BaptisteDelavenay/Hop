d07b85b611c7f56dedf1909ef981e99a20bc8434
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-03-19 16:55:13 +0100

    requete update status de la mission

---
+++ app/actions/validerMission.php
@@ -0,0 +1,18 @@
+<?php
+
+    include("../../connexionBDD/connexionBDD.php");
+
+    $updateStatus = "UPDATE mission_assign SET statut = 'validee' WHERE id = :idMission;";
+    $status = $db->prepare($updateStatus);
+    $status->execute(array(
+        "idMission" => $_POST["missionID"]
+    ));
+
+    if ($status) {
+        echo json_encode(["success"=>true]);
+    }
+    else{
+        echo json_encode(["success"=>false]);
+    }
+
+?>
