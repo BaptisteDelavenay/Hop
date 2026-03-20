b92a763d172b831c01d11ef934a89b8443483324
Baptiste Delavenay <bdelavenay78@gmail.com>
2026-03-20 15:50:44 +0100

    correction js

--- JS/barreProgression.js
+++ JS/barreProgression.js
@@ -7,12 +7,6 @@ PointsPalier.dataset.palier = PointsPalier.dataset.palier.replaceAll("/", "");
 let progression = PointsUtilisateur.dataset.progression;
 let palier = PointsPalier.dataset.palier;
 
-console.log(progression);
-console.log(palier);
-
 pourcentage = (progression * 100) / palier;
 
-console.log(Math.floor(pourcentage), "%");
-
-// Au lieu de classList.add, on modifie directement le style CSS
 BarreProgression.style.width = Math.floor(pourcentage) + "%";
